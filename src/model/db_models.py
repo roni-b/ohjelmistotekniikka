5245d52929ad8c853a8f3a1d59f7db255916a74d
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-05-12 14:16:48 +0300

    Fix

--- src/model/db_models.py
+++ src/model/db_models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Table, exc
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 import bcrypt
-from config import DATABASE_FILENAME
 
 Base = declarative_base()
 
@@ -42,7 +41,7 @@ class Quote(Base):
     def __repr__(self):
         return f"quote: {self.qid} {self.content} {self.author} {self.tags}"
 
-engine = create_engine(f"sqlite:///src/model/{DATABASE_FILENAME}")
+engine = create_engine(f"sqlite:///src/model/database.db")
 
 session_maker = sessionmaker(bind=engine)
 
